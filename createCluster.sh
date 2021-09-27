32e7935bc73b0e2197a758a89004a81fbcc85a09
Aecio Pires <aecio.pires@sensedia.com>
2021-09-27 19:30:13 -0300

    Improvements

--- createCluster.sh
+++ createCluster.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -x
+
+# Script for creating self-signed certificates.
 ./certs.sh
-# Gen cluster name
+
+# Generate cluster name
 CLUSTER_NAME=cluster-$(openssl rand -hex 3)
+
 # Create cluster
 kind create cluster --name $CLUSTER_NAME --config cluster.yaml
+
 # Get default gateway interface
 KIND_ADDRESS=$(docker network inspect kind | jq '.[].IPAM | .Config | .[0].Subnet' | cut -d \" -f 2 | cut -d"." -f1-3)
+
 # Create address range
 KIND_LB_RANGE=$(echo $KIND_ADDRESS.100)
-# Transform IP to Hex
+
+# Transform IP address to Hexadecimal
 IP_HEX=$(echo $KIND_LB_RANGE | awk -F '.' '{printf "%08x", ($1 * 2^24) + ($2 * 2^16) + ($3 * 2^8) + $4}')
+
 # Ingress Address
 KIND_INGRESS_ADDRESS=$(echo $IP_HEX.nip.io)
 
@@ -17,7 +25,7 @@ KIND_INGRESS_ADDRESS=$(echo $IP_HEX.nip.io)
 ## Linkerd
 sed "s/\$linkerd_ingress_host/linkerd.$KIND_INGRESS_ADDRESS/g" custom/templates/linkerd-viz.tpl > custom/linkerd-viz.yaml
 
-# Install repos
+# Install and upgrade Helm repositories
 helm repo add projectcalico https://docs.projectcalico.org/charts
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add metallb https://metallb.github.io/metallb
