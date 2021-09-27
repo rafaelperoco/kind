79ad51aad20b377edfc1a88494dd6cc4bbd0cab2
Rafael Peroco <rafaelperoco@gmail.com>
2021-09-27 17:39:13 -0300

    fix and changes - default cluster size - Randomize IP range - Removed cert.sh (old file)

--- createCluster.sh
+++ createCluster.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
-./certs.sh
 # Gen cluster name
 CLUSTER_NAME=cluster-$(openssl rand -hex 3)
 # Create cluster
 kind create cluster --name $CLUSTER_NAME --config cluster.yaml
 # Get default gateway interface
 KIND_ADDRESS=$(docker network inspect kind | jq '.[].IPAM | .Config | .[0].Subnet' | cut -d \" -f 2 | cut -d"." -f1-3)
+# Radomize LB IP RANGE
+KIND_ADDRESS_END=$(shuf -i 100-150 -n1)
 # Create address range
-KIND_LB_RANGE=$(echo $KIND_ADDRESS.100)
+KIND_LB_RANGE=$(echo $KIND_ADDRESS.$KIND_ADDRESS_END)
 # Transform IP to Hex
 IP_HEX=$(echo $KIND_LB_RANGE | awk -F '.' '{printf "%08x", ($1 * 2^24) + ($2 * 2^16) + ($3 * 2^8) + $4}')
 # Ingress Address
